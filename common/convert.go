8b7c66bf43997deb9d54141c67b4b8a8d767fb96
Hugo <jhugodc@gmail.com>
2023-02-14 11:19:10 -0700

    mnem2evm: accept labels

--- common/convert.go
+++ common/convert.go
@@ -7,6 +7,21 @@ import (
 	"strings"
 )
 
+const (
+	Value int = iota
+	Label
+)
+
+type Immediate struct {
+	Type      int
+	Immediate string
+}
+
+type OpCall struct {
+	OpCode
+	Immediates []Immediate
+}
+
 func Evm2Mnem(bytecode string) string {
 	result := ""
 	opcodes := GetOpcodesByNumber()
@@ -48,56 +63,53 @@ func Evm2Mnem(bytecode string) string {
 	return result
 }
 
-func opcode2evm(opcode string, immediate string) (string, error) {
+func opcode2evm(opcode string, immediate string) (OpCall, error) {
 	opcodes := GetOpcodesByName()
 	immediate = strings.ToLower(immediate)
 
 	op := opcodes[opcode]
+	opCall := OpCall{OpCode: op, Immediates: make([]Immediate, 0)}
 
 	if op.Name == "" {
 		errmsg := "Opcode not assigned: " + opcode
-		return "", errors.New(errmsg)
+		return opCall, errors.New(errmsg)
 	}
 
 	if immediate == "" && op.Immediates > 0 {
-		return "", errors.New("Immediate expected for opcode")
+		return opCall, errors.New("Immediate expected for opcode")
 	}
 
 	if immediate != "" && op.Immediates == 0 {
-		return "", errors.New("Immediate NOT expected for opcode")
-	}
-
-	result := strconv.FormatInt(int64(op.Code), 16)
-	if len(result)%2 != 0 {
-		result = "0" + result
+		return opCall, errors.New("Immediate NOT expected for opcode")
 	}
 
 	if immediate != "" {
 		if op.Name == "RJUMPV" {
 			values := strings.Split(immediate, ",")
 			count := len(values)
-			result += fmt.Sprintf("%02x", count)
+			opCall.Immediates = append(opCall.Immediates, Immediate{Type: Value, Immediate: fmt.Sprintf("%02x", count)})
 			for _, ro := range values {
 				relativeOffset, err := strconv.ParseInt(ro, 10, 64)
 
 				if err != nil {
-					return "", errors.New("Error parsing RJUMPV relative offset")
-				}
+					opCall.Immediates = append(opCall.Immediates, Immediate{Type: Label, Immediate: ro})
+				} else {
 
-				if relativeOffset < 0 {
-					relativeOffset = 65535 - (relativeOffset * -1) + 1
+					if relativeOffset < 0 {
+						relativeOffset = 65535 - (relativeOffset * -1) + 1
+					}
+					opCall.Immediates = append(opCall.Immediates, Immediate{Type: Value, Immediate: fmt.Sprintf("%04x", relativeOffset)})
 				}
-				result += fmt.Sprintf("%04x", relativeOffset)
 			}
 
-			return result, nil
+			return opCall, nil
 		}
 
 		// For >push2 a hexadecimal must be received as parameter
 		imm_hex := ""
 		if op.Code > 0x60 && op.Code <= 0x7f {
 			if immediate[0:2] != "0x" {
-				return "", errors.New("hexadecimal value expected")
+				return opCall, errors.New("hexadecimal value expected")
 			}
 			immediate = immediate[2:]
 			totalBytes := op.Code - 0x5f
@@ -113,44 +125,49 @@ func opcode2evm(opcode string, immediate string) (string, error) {
 		} else {
 			imm, err := strconv.ParseInt(immediate, 10, 64)
 			if err != nil {
-				return "", err
-			}
-
-			if imm < 0 {
-				if op.Name != "RJUMP" && op.Name != "RJUMPI" {
-					return "", errors.New("Negative immediate only possible for RJUMP and RJUMPI")
-				}
-
-				imm_hex = strconv.FormatUint(uint64(imm), 16)
-				imm_hex = imm_hex[len(imm_hex)-op.Immediates*2:]
+				opCall.Immediates = append(opCall.Immediates, Immediate{Type: Label, Immediate: immediate})
 			} else {
-				imm_hex = strconv.FormatInt(imm, 16)
-				if len(imm_hex)%2 != 0 {
-					imm_hex = "0" + imm_hex
-				}
-				for {
-					if len(imm_hex) < op.Immediates*2 {
-						imm_hex = "00" + imm_hex
-					} else {
-						break
+				if imm < 0 {
+					if op.Name != "RJUMP" && op.Name != "RJUMPI" {
+						return opCall, errors.New("Negative immediate only possible for RJUMP and RJUMPI")
 					}
+
+					imm_hex = strconv.FormatUint(uint64(imm), 16)
+					imm_hex = imm_hex[len(imm_hex)-op.Immediates*2:]
+					opCall.Immediates = append(opCall.Immediates, Immediate{Type: Value, Immediate: imm_hex})
+				} else {
+					imm_hex = strconv.FormatInt(imm, 16)
+					if len(imm_hex)%2 != 0 {
+						imm_hex = "0" + imm_hex
+					}
+					for {
+						if len(imm_hex) < op.Immediates*2 {
+							imm_hex = "00" + imm_hex
+						} else {
+							break
+						}
+					}
+					opCall.Immediates = append(opCall.Immediates, Immediate{Type: Value, Immediate: imm_hex})
 				}
 			}
 		}
-
-		result = result + imm_hex
 	}
-
-	return result, nil
+	return opCall, nil
 }
 
 func Mnem2Evm(mn string) string {
 	tokens := strings.Split(mn, " ")
-	result := ""
 
+	labels := make(map[string]int)
+	evm := make([]OpCall, 0)
+	pos := 0
 	for _, token := range tokens {
 		token = strings.Trim(token, " ")
 		if token == "" {
+			continue
+		}
+		if token[len(token)-1] == ':' {
+			labels[token[:len(token)-1]] = pos
 			continue
 		}
 		opcode := ""
@@ -162,14 +179,45 @@ func Mnem2Evm(mn string) string {
 		} else {
 			opcode = token
 		}
-
-		evm, err := opcode2evm(opcode, immediate)
+		opCall, err := opcode2evm(opcode, immediate)
 
 		if err != nil {
 			fmt.Println("Error: ", err, opcode, immediate)
 			return ""
 		}
-		result = result + evm
+		evm = append(evm, opCall)
+
+		pos = pos + 1 + opCall.OpCode.Immediates
+	}
+
+	result := ""
+	pos = 0
+	for _, op := range evm {
+		result += op.OpCode.AsHex()
+
+		pos = pos + 1 + op.OpCode.Immediates
+		for _, im := range op.Immediates {
+			if im.Type == Label {
+				if p, ok := labels[im.Immediate]; ok {
+					if op.OpCode.Name == "RJUMP" || op.OpCode.Name == "RJUMPI" || op.OpCode.Name == "RJUMPV" {
+						if p > pos {
+							result += fmt.Sprintf("%04x", p-pos)
+						} else {
+							imm_hex := strconv.FormatUint(uint64(p-pos), 16)
+							imm_hex = imm_hex[len(imm_hex)-op.OpCode.Immediates*2:]
+							result += imm_hex
+						}
+					} else {
+						result += fmt.Sprintf("%04x", p)
+					}
+				} else {
+					fmt.Println("Error: Label not found: ", im)
+					fmt.Println("labels: ", labels)
+				}
+			} else {
+				result += im.Immediate
+			}
+		}
 	}
 
 	return result
