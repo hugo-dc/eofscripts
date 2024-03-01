fd1569b510a2e8c5a86418bcb4c1f7c6b89f5d7f
Hugo De La Cruz <jhugodc@gmail.com>
2024-03-01 00:28:45 -0700

    Validate input parameters

--- cmd/opinfo/main.go
+++ cmd/opinfo/main.go
@@ -11,39 +11,33 @@ import (
 func showUsage() {
 	fmt.Println("opinfo <opcode> [option]")
 	fmt.Println("  opcode:")
-	fmt.Println("    <opcode>\tOpcode name, decimal, or hexadecimal value (0x prefixed)")
+	fmt.Println("    <opcode>\t\tOpcode name, decimal, or hexadecimal value (0x prefixed)")
 	fmt.Println("  options:")
-	fmt.Println("    --name\tShow opcode Name")
-	fmt.Println("    --hex\tShow opcode hexadecimal value")
-	fmt.Println("    --inputs\tShow required stack items")
-	fmt.Println("    --outputs\tShow pushed stack items")
-	fmt.Println("    --ispush\tReturns true if opcode is a PUSH opcode")
+	fmt.Println("    --name\t\tShow opcode Name")
+	fmt.Println("    --hex\t\tShow opcode hexadecimal value")
+	fmt.Println("    --inputs\t\tShow required stack items")
+	fmt.Println("    --outputs\t\tShow pushed stack items")
+	fmt.Println("    --is-push\t\tReturns true if opcode is a PUSH opcode")
 	fmt.Println("    --immediates\tReturns the number of bytes the opcodes requires as immediates")
 	fmt.Println("    --is-terminating\tReturns true if opcode is a terminating opcode")
 }
 
 func main() {
-
 	if len(os.Args) != 3 {
 		showUsage()
 		return
 	}
 
 	opId := os.Args[1]
-
 	var opcode common.OpCode
-
 	if opId[:2] == "0x" {
-
 		opId64, err := strconv.ParseInt(opId[2:], 16, 64)
-
 		if err != nil {
 			fmt.Println("Error:", err)
 			return
 		}
 
 		opcodes := common.GetOpcodesByNumber()
-
 		if op, ok := opcodes[int(opId64)]; ok {
 			opcode = op
 		} else {
@@ -52,10 +46,8 @@ func main() {
 		}
 	} else {
 		opId64, err := strconv.ParseInt(opId, 10, 64)
-
 		if err != nil {
 			opcodes := common.GetOpcodesByName()
-
 			if op, ok := opcodes[opId]; ok {
 				opcode = op
 			} else {
@@ -65,7 +57,6 @@ func main() {
 		} else {
 			fmt.Println("⚠️ Warning: opcode value", opId, "is being interpreted as decimal\n")
 			opcodes := common.GetOpcodesByNumber()
-
 			if op, ok := opcodes[int(opId64)]; ok {
 				opcode = op
 			}
@@ -73,36 +64,30 @@ func main() {
 	}
 
 	option := os.Args[2]
-	if option == "--name" {
+	switch option {
+	case "--name":
 		fmt.Println(opcode.Name)
-	}
-	if option == "--hex" {
+	case "--hex":
 		fmt.Printf("%02x\n", opcode.Code)
-	}
-	if option == "--inputs" {
+	case "--inputs":
 		fmt.Println(opcode.StackInput)
-	}
-	if option == "--outputs" {
+	case "--outputs":
 		fmt.Println(opcode.StackOutput)
-	}
-	if option == "--ispush" {
+	case "--is-push":
 		if opcode.Code >= 0x60 && opcode.Code <= 0x7f {
 			fmt.Println("true")
 		} else {
 			fmt.Println("false")
 		}
-	}
-	if option == "--immediates" {
+	case "--immediates":
 		fmt.Println(opcode.Immediates)
-	}
-	if option == "--is-terminating" {
+	case "--is-terminating":
 		if opcode.IsTerminating {
 			fmt.Println("true")
 		} else {
 			fmt.Println("false")
 		}
-	}
-	if option == "--all" {
+	case "--all":
 		fmt.Println("Name:", opcode.Name)
 		fmt.Println("Hex:", fmt.Sprintf("0x%02x", opcode.Code))
 		fmt.Println("Inputs:", opcode.StackInput)
@@ -110,5 +95,7 @@ func main() {
 		fmt.Println("IsPush:", opcode.IsPush)
 		fmt.Println("Immediates:", opcode.Immediates)
 		fmt.Println("IsTerminating:", opcode.IsTerminating)
+	default:
+		showUsage()
 	}
 }
